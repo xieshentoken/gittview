8de1135226aa34dd8b0c140bd9d7b557ba1a22fd
xieshentoken <47736607+xieshentoken@users.noreply.github.com>
2020-07-28 21:55:03 +0800

    Add files via upload

--- process.py
+++ process.py
@@ -16,48 +16,63 @@ class Gitt():
         ll = len(pristine_data) - 1
         priQ = pristine_data['比容量/mAh/g']
         priI = pristine_data['电流/mA']
-        for x in range(ll):
-            if (priQ.iloc[x] > 0)and(priQ.iloc[x+1] == 0)and(priI.iloc[x+1] > 0):
-                cd = 0
-                dc = x + 1
-                #  将充电放电数据分别存在两个DataFrame中，并统一index，重新计算测试时间
-                self.discharge_data = pristine_data.iloc[:dc]
-                self.discharge_data.index = np.arange(len(self.discharge_data))
-                dischargeT = self.discharge_data['测试时间/Sec']
-                self.discharge_data['测试时间/Sec'] = self.discharge_data['测试时间/Sec'] - dischargeT.iloc[0]
-                self.charge_data = pristine_data.iloc[dc:]
-                self.charge_data.index = np.arange(len(self.charge_data))
-                chargeT = self.charge_data['测试时间/Sec']
-                self.charge_data['测试时间/Sec'] = self.charge_data['测试时间/Sec'] - chargeT.iloc[0]
-                break
-            elif (priQ.iloc[x] > 0)and(priQ.iloc[x+1] == 0)and(priI.iloc[x+1] < 0):
-                cd = x + 1
-                dc = 0
-                self.discharge_data = pristine_data.iloc[cd:]
-                self.discharge_data.index = np.arange(len(self.discharge_data))
-                dischargeT = self.discharge_data['测试时间/Sec']
-                self.discharge_data['测试时间/Sec'] = self.discharge_data['测试时间/Sec'] - dischargeT.iloc[0]
-                self.charge_data = pristine_data.iloc[:cd]
-                self.charge_data.index = np.arange(len(self.charge_data))
-                chargeT = self.charge_data['测试时间/Sec']
-                self.charge_data['测试时间/Sec'] = self.charge_data['测试时间/Sec'] - chargeT.iloc[0]
-                break
+        if priI.max()*priI.min() < 0:
+            for x in range(ll):
+                if (priQ.iloc[x] > 0)and(priQ.iloc[x+1] == 0)and(priI.iloc[x+1] > 0):
+                    cd = 0
+                    dc = x + 1
+                    #  将充电放电数据分别存在两个DataFrame中，并统一index，重新计算测试时间
+                    self.discharge_data = pristine_data.iloc[:dc]
+                    self.discharge_data.index = np.arange(len(self.discharge_data))
+                    dischargeT = self.discharge_data['测试时间/Sec']
+                    self.discharge_data['测试时间/Sec'] = self.discharge_data['测试时间/Sec'] - dischargeT.iloc[0]
+                    self.charge_data = pristine_data.iloc[dc:]
+                    self.charge_data.index = np.arange(len(self.charge_data))
+                    chargeT = self.charge_data['测试时间/Sec']
+                    self.charge_data['测试时间/Sec'] = self.charge_data['测试时间/Sec'] - chargeT.iloc[0]
+                    break
+                elif (priQ.iloc[x] > 0)and(priQ.iloc[x+1] == 0)and(priI.iloc[x+1] < 0):
+                    cd = x + 1
+                    dc = 0
+                    self.discharge_data = pristine_data.iloc[cd:]
+                    self.discharge_data.index = np.arange(len(self.discharge_data))
+                    dischargeT = self.discharge_data['测试时间/Sec']
+                    self.discharge_data['测试时间/Sec'] = self.discharge_data['测试时间/Sec'] - dischargeT.iloc[0]
+                    self.charge_data = pristine_data.iloc[:cd]
+                    self.charge_data.index = np.arange(len(self.charge_data))
+                    chargeT = self.charge_data['测试时间/Sec']
+                    self.charge_data['测试时间/Sec'] = self.charge_data['测试时间/Sec'] - chargeT.iloc[0]
+                    break
+        elif (priI.max()*priI.min() == 0)and(priI.max() > 0):
+                    self.charge_data = pristine_data.iloc[:]
+                    self.charge_data.index = np.arange(len(self.charge_data))
+                    chargeT = self.charge_data['测试时间/Sec']
+                    self.charge_data['测试时间/Sec'] = self.charge_data['测试时间/Sec'] - chargeT.iloc[0]
+                    self.discharge_data = self.charge_data
+        elif (priI.max()*priI.min() == 0)and(priI.min() < 0):
+                    self.discharge_data = pristine_data.iloc[:]
+                    self.discharge_data.index = np.arange(len(self.discharge_data))
+                    dischargeT = self.discharge_data['测试时间/Sec']
+                    self.discharge_data['测试时间/Sec'] = self.discharge_data['测试时间/Sec'] - dischargeT.iloc[0]
+                    self.charge_data = self.discharge_data
 
     def diffus_fit(self, data, tao, mass, area, density, DROP, customize_Constant):
         length0 = len(data)-1
         priU = data['电压/V']
         priI = data['电流/mA']
         capacity = data['比容量/mAh/g']
-        E_tao = priU.loc[[x for x in range(length0) if ((priI.loc[x] != 0)and(priI.loc[x+1] == 0))or(x == length0-1)]]
+        E_tao = priU.loc[[x for x in range(length0) if ((priI.loc[x] != 0)and(priI.loc[x+1] == 0))or((x == length0-1)and(priI.loc[x] != 0))]]
         E_s = priU.loc[[x for x in range(length0) if ((priI.loc[x] == 0)and(priI.loc[x+1] != 0))or(capacity.loc[x] == 0)]]
-        E_R = priU.loc[[x+customize_Constant for x in range(length0) if ((priI.loc[x] == 0)and(priI.loc[x+1] != 0))or(capacity.loc[x] == 0)]]
+        E_R = priU.loc[[x+customize_Constant for x in range(length0-customize_Constant) if ((priI.loc[x] == 0)and(priI.loc[x+1] != 0))or(capacity.loc[x] == 0)]]
         deta_Es = E_s.diff().dropna()   # 前向差分并丢弃第一个空值
+        # E_s.to_csv(r"C:\Users\Administrator\Desktop\Es.csv")
+        # E_tao.to_csv(r"C:\Users\Administrator\Desktop\Etao.csv")
         if DROP == 0:
             deta_Etao = E_tao.iloc[:DROP-1].values - E_R.iloc[:DROP-1].values
-            Q = capacity.loc[[x for x in range(length0) if ((priI.loc[x] != 0)and(priI.loc[x+1] == 0))or(x == length0-1)]].iloc[:DROP-1]
+            Q = capacity.loc[[x for x in range(length0) if ((priI.loc[x] == 0)and(priI.loc[x+1] != 0))or(capacity.loc[x] == 0)]].iloc[:DROP-1]
         elif DROP == 1:
             deta_Etao = E_tao.iloc[DROP:].values - E_R.iloc[DROP:].values    
-            Q = capacity.loc[[x for x in range(length0) if ((priI.loc[x] != 0)and(priI.loc[x+1] == 0))or(x == length0-1)]].iloc[DROP:]
+            Q = capacity.loc[[x for x in range(length0) if ((priI.loc[x] == 0)and(priI.loc[x+1] != 0))or(capacity.loc[x] == 0)]].iloc[DROP:]
         index = np.arange(len(deta_Es))
         U = E_s.iloc[:-1]
         U.index = index
